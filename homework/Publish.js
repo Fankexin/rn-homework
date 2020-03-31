b183e2d5d611a79a28ad9699eee0e065d9a8ac7a
Fankexin <974094944@qq.com>
2020-03-31 11:13:16 +0800

    .

--- homework/Publish.js
+++ homework/Publish.js
@@ -3,7 +3,8 @@ import {
     View,
     Text,
     ToastAndroid,
-    SafeAreaView
+    SafeAreaView,
+    ScrollView
 } from 'react-native';
 import Button from 'react-native-button';
 import { Actions } from 'react-native-router-flux';
@@ -20,7 +21,7 @@ export default class Publish extends Component {
     }
     fetchData = (page) => {
         this.setState({ isloading: true })
-        fetch('https://cnodejs.org/api/v1/topics?limit=11&page=' + page)
+        fetch('https://cnodejs.org/api/v1/topics?limit=12&page=' + page)
             .then((res) => res.json())
             .then((res) => {
                 this.setState({ isloading: false });
@@ -51,79 +52,83 @@ export default class Publish extends Component {
     }
     render() {
         return (
-            <SafeAreaView>
+            <SafeAreaView  style={{ flex: 1 }}>
                 <View style={{ width: '100%', height: 50, justifyContent: 'space-between', backgroundColor: '#f23030', flexDirection: 'row', alignItems: 'center' }}>
                     <Icon style={{ marginLeft: 10 }} color='#fff' name='left' onPress={() => Actions.pop()} />
-                    <Text style={{ fontSize: 20, color: '#fff', marginLeft: 150, marginRight: 150 }}>我的发布</Text>
+                    <Text style={{ fontSize: 20, color: '#fff' }}>我的发布</Text>
                     <Icon style={{ marginRight: 10 }} color='#fff' name='ellipsis' />
                 </View>
-                <View style={{ marginTop: 6 }}>
-                    {
-                        this.state.tits.map((item) => (
-                            <View style={{
-                                flexDirection: 'row',
-                                justifyContent: 'space-between',
-                                alignItems: 'center',
-                                height: 46,
-                                paddingLeft: 6,
-                                backgroundColor: '#fff',
-                                borderBottomColor: '#f5f5f5',
-                                borderBottomWidth: 1,
-                                borderBottomStyle: 'dashed'
-                            }}>
-                                <Text style={{ fontSize: 15, color: '#333333' }}>{item.title}</Text>
-                                <View style={{
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                    width: 160,
-                                    paddingRight: 14
-                                }}>
-                                    <Text style={{ fontSize: 15, color: '#333333', marginRight: 20 }}>{item.create_at}</Text>
-                                    <Text style={item.num === '待回复' ? { color: 'red', fontSize: 15 } : { fontSize: 15, color: '#333333' }}>{item.num}</Text>
-                                </View>
-                            </View>
-                        ))
-                    }
-                </View>
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    backgroundColor: '#fff',
-                    paddingTop: 20,
-                    paddingBottom: 20,
-                    paddingLeft: 6,
-                    paddingRight: 6
-                }}>
-                    <Button
-                        onPress={this.prev}
-                        style={{
-                            width: 105,
-                            height: 35,
-                            borderRadius: 20,
-                            textAlignVertical: 'center',
-                            backgroundColor: 'red',
-                            color: '#fff'
-                        }}
-                    >上一页</Button>
-                    <Text>第{this.state.page}页</Text>
-                    <Button
-                        onPress={this.next}
-                        style={{
-                            width: 105,
-                            height: 35,
-                            borderRadius: 20,
-                            textAlignVertical: 'center',
-                            backgroundColor: 'red',
-                            color: '#fff'
-                        }}
-                    >下一页</Button>
-                </View>
-                {
-                    this.state.isloading
-                        ? <View style={{ alignItems: 'center' }}><Text style={{ fontSize: 20,marginTop:10 }}>正在获取数据...</Text></View>
-                        : null
-                }
+                <ScrollView style={{ flex: 1 }}>
+                    <View>
+                        <View style={{ marginTop: 6 }}>
+                            {
+                                this.state.tits.map((item) => (
+                                    <View style={{
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                        height: 46,
+                                        paddingLeft: 6,
+                                        backgroundColor: '#fff',
+                                        borderBottomColor: '#f5f5f5',
+                                        borderBottomWidth: 1,
+                                        borderBottomStyle: 'dashed'
+                                    }}>
+                                        <Text style={{ fontSize: 15, color: '#333333' }}>{item.title}</Text>
+                                        <View style={{
+                                            flexDirection: 'row',
+                                            justifyContent: 'space-between',
+                                            width: 160,
+                                            paddingRight: 14
+                                        }}>
+                                            <Text style={{ fontSize: 15, color: '#333333', marginRight: 20 }}>{item.create_at}</Text>
+                                            <Text style={item.num === '待回复' ? { color: 'red', fontSize: 15 } : { fontSize: 15, color: '#333333' }}>{item.num}</Text>
+                                        </View>
+                                    </View>
+                                ))
+                            }
+                        </View>
+                        <View style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            backgroundColor: '#fff',
+                            paddingTop: 20,
+                            paddingBottom: 20,
+                            paddingLeft: 6,
+                            paddingRight: 6
+                        }}>
+                            <Button
+                                onPress={this.prev}
+                                style={{
+                                    width: 105,
+                                    height: 35,
+                                    borderRadius: 20,
+                                    textAlignVertical: 'center',
+                                    backgroundColor: 'red',
+                                    color: '#fff'
+                                }}
+                            >上一页</Button>
+                            <Text>第{this.state.page}页</Text>
+                            <Button
+                                onPress={this.next}
+                                style={{
+                                    width: 105,
+                                    height: 35,
+                                    borderRadius: 20,
+                                    textAlignVertical: 'center',
+                                    backgroundColor: 'red',
+                                    color: '#fff'
+                                }}
+                            >下一页</Button>
+                        </View>
+                        {
+                            this.state.isloading
+                                ? <View style={{ alignItems: 'center' }}><Text style={{ fontSize: 20, marginTop: 10 }}>正在获取数据...</Text></View>
+                                : null
+                        }
+                    </View>
+                </ScrollView>
             </SafeAreaView>
         )
     }
